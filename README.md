0f9911da866f966966d12f69a7534f40b9c55e13
hritikdangi3 <hritikdangi3@gmail.com>
2021-09-26 01:30:38 +0530

    Mark7

---
+++ README.md
@@ -0,0 +1,6 @@
+README.md
+
+ 
+ We all have seen phineas and ferb in our childhood, ya ya some of you still watch it,lol
+
+ Try this translator to talk like Ferb, isn't it cool try it now
---
+++ script.js
@@ -0,0 +1,26 @@
+
+var  btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+
+var url = "https://api.funtranslations.com/translate/ferb-latin.json"
+function gettranslated(input){
+  return url + "?" +"text=" + input
+}
+function errorhandler(){
+ alert("Milion lost in space,server is facing delay please try again in some time")
+}
+function clickhandler(){
+  var inputxt = txtInput.value
+  fetch(gettranslated(inputxt))
+  .then (response => response.json())
+  .then  (json => {
+    var translatedtext = json.contents.translated;
+    outputDiv.innerText = translatedtext;
+  })
+  .Catch(errorhandler)
+  console.log(errorhandler)
+};
+btnTranslate.addEventListener("click", clickhandler)
